6bda89ee910f79e18045bb1474ce13b89e75e775
xander-che <achernys123@gmail.com>
2024-09-25 20:05:35 +0300

    2 commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from './schedule/schedule.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ScheduleModule, RoomsModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
