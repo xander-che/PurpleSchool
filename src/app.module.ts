aeafde991fa8c0b7d5488636eed29927ed419074
xander-che <achernys123@gmail.com>
2024-10-03 18:23:03 +0300

    mongo changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { ScheduleModule } from './schedule/schedule.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { RoomsController } from './rooms/rooms.controller';
 import { ScheduleController } from './schedule/schedule.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [ScheduleModule, RoomsModule],
+	imports: [
+		ScheduleModule,
+		RoomsModule,
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot('mongodb://localhost/sadb'),
+	],
 	controllers: [AppController, RoomsController, ScheduleController],
 	providers: [AppService],
 })
